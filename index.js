a2e265254d1cd7a95f2242d9e58d46667d4629b7
Rimantas <belauzas@yahoo.com>
2022-03-24 19:43:35 +0200

    Initial File functions

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('file-system');
+import { file } from './file.js';
+
+console.log(file);
+
+const status = file.create();
+file.read();
+file.update();
+file.read();
+file.delete();
+file.read();
