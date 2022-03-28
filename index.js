a8511545f931f24abb6151b98a4500c614a50954
Rimantas <belauzas@yahoo.com>
2022-03-28 18:57:14 +0300

    Update file

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 import { file } from './lib/file.js';
 
 const createFileStatus = await file.create('users', 'petras.json', {
-    name: 'Petras'
+    name: 'Petras',
+    age: 99,
+    isMarried: true
 });
 console.log(createFileStatus);
 
 const readFileStatus1 = await file.read('users', 'petras.json');
 console.log(readFileStatus1);
 
-file.update();
+const updateFileStatus = await file.update('users', 'petras.json', {
+    name: 'Petras',
+    age: 99
+});
+console.log(updateFileStatus);
 
 const readFileStatus2 = await file.read('users', 'petras.json');
 console.log(readFileStatus2);
 
-file.delete();
+const updateFileStatus2 = await file.update('users', 'petras.json', {
+    name: 'Petras'
+});
+console.log(updateFileStatus2);
 
 const readFileStatus3 = await file.read('users', 'petras.json');
 console.log(readFileStatus3);
+
+// file.delete();
+
+// const readFileStatus3 = await file.read('users', 'petras.json');
+// console.log(readFileStatus3);
 
 
