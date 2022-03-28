48c11e2e02901c109a8093134befe579eb3ccd43
Rimantas <belauzas@yahoo.com>
2022-03-28 20:30:36 +0300

    Users 2

--- users.js
+++ users.js
@@ -1,5 +1,16 @@
-const users = ['Petras', 'Maryte', 'Jonas', 'Ona'];
-// 1) Studetas: Petras.
-// 2) Studetas: Maryte.
-// 3) Studetas: Jonas.
-// 4) Studetas: Ona.
+// const users = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+
+const users = [
+    { name: 'Petras', age: 99, isMarried: true },
+    { name: 'Maryte', age: 87, isMarried: false },
+    { name: 'Jonas', age: 66, isMarried: false },
+    { name: 'Ona', age: 54, isMarried: true },
+];
+
+let i = 0;
+for (const user of users) {
+    console.log(`${++i}) Studentas: ${user}.`);
+}
+
+// 1) Student Petras is 99 years old and is married.
+// 2) Student Maryte is 87 years old and is not married.
